c11e62c9cbb7c30ebe0b5db0022e7b40060af16e
Wioleta Ligęza <wligeza881@gmail.com>
2026-06-14 20:09:03 +0200

    06 CRUD zadanie 1

---
+++ 06 CRUD z bazą danych/TravelQuotesApi/Controllers/QuotesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelQuotesApi.Data;
+using TravelQuotesApi.Interfaces;
+using TravelQuotesApi.Models;
+
+[Route("api/[controller]")]
+[ApiController]
+public class QuotesController : ControllerBase
+{
+    private readonly IRepository<Quote> _quoteRepository;
+
+    public QuotesController(IRepository<Quote> quoteRepository)
+    {
+        _quoteRepository = quoteRepository;
+    }
+
+    // GET: api/Quotes
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Quote>>> GetQuotes()
+    {
+        var quotes = await _quoteRepository.GetAllAsync();
+        return Ok(quotes);
+    }
+
+    // POST: api/Quotes
+    [HttpPost]
+    public async Task<ActionResult<Quote>> PostQuote(Quote quote)
+    {
+        quote.Id = 0;
+        await _quoteRepository.CreateAsync(quote);
+        return Ok(quote);
+    }
+}
